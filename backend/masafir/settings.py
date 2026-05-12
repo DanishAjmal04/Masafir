9e5726a7c67c992cda28dec5ce732698f8c716cf
Danish Ajmal <danishajmal56da@gmail.com>
2026-05-13 00:40:35 +0500

    Fix database config

--- backend/masafir/settings.py
+++ backend/masafir/settings.py
@@ -112,8 +112,8 @@ WSGI_APPLICATION = 'masafir.wsgi.application'
 # =========================
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+    "default": dj_database_url.parse(
+        os.environ["DATABASE_URL"],
         conn_max_age=600,
     )
 }
