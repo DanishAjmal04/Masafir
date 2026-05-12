15d146663b274397873467fad7cab2e4b548a8a3
Danish Ajmal <danishajmal56da@gmail.com>
2026-05-13 04:48:10 +0500

    fix: increase upload size limit to 20MB

--- backend/masafir/settings.py
+++ backend/masafir/settings.py
@@ -122,14 +122,12 @@ else:
 # =========================
 # PASSWORD VALIDATION
 # =========================
-
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
     {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
-
 # =========================
 # INTERNATIONALIZATION
 # =========================
