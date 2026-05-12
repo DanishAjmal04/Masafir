e251b68be566e1068c83014c1eea7471198fcb52
Danish Ajmal <danishajmal56da@gmail.com>
2026-05-13 03:37:34 +0500

    fix: increase upload size limit to 20MB

--- backend/masafir/settings.py
+++ backend/masafir/settings.py
@@ -2,7 +2,6 @@ from pathlib import Path
 from datetime import timedelta
 import os
 import dj_database_url
-import cloudinary
 from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
