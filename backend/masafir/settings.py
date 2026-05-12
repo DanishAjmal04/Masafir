53b8b86dbaf741971f61855ca0dc83d87a7a1055
Danish Ajmal <danishajmal56da@gmail.com>
2026-05-13 00:45:20 +0500

    Fix database config

--- backend/masafir/settings.py
+++ backend/masafir/settings.py
@@ -112,9 +112,8 @@ WSGI_APPLICATION = 'masafir.wsgi.application'
 # =========================
 
 DATABASES = {
-    "default": dj_database_url.parse(
-        os.environ["DATABASE_URL"],
-        conn_max_age=600,
+    "default": dj_database_url.config(
+        conn_max_age=600
     )
 }
 
